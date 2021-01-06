b3dc7888677c2e8c8393105f2799b7266d80754d
djoseitz <72769693+djoseitz@users.noreply.github.com>
2021-01-05 16:56:23 -0800

    set up movie-view.jsx and adjusted main-view to accommodate

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,44 +1,44 @@
 import React from 'react';
 import axios from 'axios';
+
 import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
 
 export class MainView extends React.Component {
 
+  constructor() {
+    super();
 
-  // Gives "movies" an initial state, since render runs before componentDidMount
-  constructor(props) {
-    super(props)
     this.state = {
-      movies: []
-    }
+      movies: null,
+      selectedMovie: null
+    };
   }
-  // One of the "hooks" available in a React Component
+
   componentDidMount() {
-    axios.get('https://cinefandb.herokuapp.com/movies')
-      .then(response => {
-        // Assign the result to the state
-        this.setState({
-          movies: response.data
-        });
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+    /* ... */
+  }
+
+  onMovieClick(movie) {
+    this.setState({
+      selectedMovie: movie
+    });
   }
 
 
   render() {
-    const { movies } = this.state;
+    const { movies, selectedMovie } = this.state;
 
     // Before the movies have been loaded
     if (!movies) return <div className="main-view" />;
 
     return (
       <div className="main-view">
-        { movies.map(movie => (
-          <MovieCard key={movie._id} movie={movie} />
-        )
-        )
+        {selectedMovie
+          ? <MovieView movie={selectedMovie} />
+          : movies.map(movie => (
+            <MovieCard key={movie._id} movie={movie} onClick={movie => this.onMovieClick(movie)} />
+          ))
         }
       </div>
     );
