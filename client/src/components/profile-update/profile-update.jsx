d518f88f88d3e7f94d423cf3eeeb13f71823e99e
djoseitz <72769693+djoseitz@users.noreply.github.com>
2021-03-24 03:50:33 -0700

    fixed styling for update user view

--- client/src/components/profile-update/profile-update.jsx
+++ client/src/components/profile-update/profile-update.jsx
@@ -83,7 +83,7 @@ export function ProfileUpdate(props) {
 
   return (
     <Container>
-      <h1>Update your account</h1>
+      <h1 color="#ede13f">Update your account</h1>
       <Form className="registration-form">
         <Form.Group controlId="formBasicUsername">
           <Form.Label>Username:</Form.Label>
